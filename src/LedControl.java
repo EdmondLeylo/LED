9462f2314bf794aa600fcca931cd2b05103bbe4c
Edmond Leylo <edmondlaylo@gmail.com>
2015-05-24 14:11:14 +0300

    123

--- src/LedControl.java
+++ src/LedControl.java
@@ -6,31 +6,31 @@ import com.pi4j.io.gpio.RaspiPin;
 
 public class LedControl {
 	
-    public static void main(String[] args) throws InterruptedException {
-    	
-        // get a handle to the GPIO controller
-    	final GpioController gpio = GpioFactory.getInstance();
+	final private GpioController gpio;
+	
+	public LedControl(){
+		gpio = GpioFactory.getInstance();
+	}
+	public void turnLED(int sec){
         
         // creating the pin with parameter PinState.HIGH
         // will instantly power up the pin
         final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "PinLED", PinState.HIGH);
         System.out.println("light is: ON");
         
-        // wait 2 seconds
-        Thread.sleep(2000);
-        
-        // turn off GPIO 1
-        pin.low();
-        System.out.println("light is: OFF");
-
-        // wait 1 second
-        Thread.sleep(1000);
-
-        // turn on GPIO 1 for 1 second and then off
-        System.out.println("light is: ON for 1 second");
-        pin.pulse(1000, true);
-        
-        // release the GPIO controller resources
-        gpio.shutdown();
+        // wait sec seconds
+        try {
+			Thread.sleep(sec *1000);
+			pin.low();
+			gpio.shutdown();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+    public static void main(String[] args) throws InterruptedException {
+    	LedControl led = new LedControl();
+    	led.turnLED(5);
     }
 }
